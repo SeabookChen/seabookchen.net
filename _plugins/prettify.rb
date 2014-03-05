5771442082fa2fd7c507c3aca9a074f0129f5c34
Yi Zeng <yi.zeng.dev@gmail.com>
2014-03-05 13:46:29 +1300

    Update prettify.rb plugin to match the modules in highlight.rb, case #18

--- _plugins/prettify.rb
+++ _plugins/prettify.rb
@@ -1,34 +1,34 @@
 module Jekyll
+  module Tags
+    class PrettifyBlock < Liquid::Block
+      include Liquid::StandardFilters
 
-  class PrettifyBlock < Liquid::Block
-    include Liquid::StandardFilters
+      SYNTAX = %r{^([a-zA-Z0-9.+#-])*$}
 
-    SYNTAX = %r{^([a-zA-Z0-9.+#-])*$}
+      def initialize(tag_name, markup, tokens)
+        super
+        if markup.strip =~ SYNTAX
+            @lang = " lang-#{markup.strip.downcase}"
+        else
+            raise SyntaxError.new <<-eos
+  Syntax Error in tag 'prettify' while parsing the following markup:
 
-    def initialize(tag_name, markup, tokens)
-      super
-      if markup.strip =~ SYNTAX
-          @lang = " lang-#{markup.strip.downcase}"
-      else
-          raise SyntaxError.new <<-eos
-Syntax Error in tag 'prettify' while parsing the following markup:
+    #{markup}
 
-  #{markup}
+  Valid syntax: prettify [lang]
+  eos
+        end
+      end
 
-Valid syntax: prettify [lang]
-eos
+      def render(context)
+        code = h(super).strip
+
+        <<-HTML
+<div><pre class="prettyprint#{@lang}"><code>#{code}</code></pre></div>
+        HTML
       end
     end
-
-    def render(context)
-      code = h(super).strip
-
-      <<-HTML
-<div><pre class="prettyprint#{@lang}"><code>#{code}</code></pre></div>
-      HTML
-    end
   end
-
 end
 
-Liquid::Template.register_tag('prettify', Jekyll::PrettifyBlock)
+Liquid::Template.register_tag('prettify', Jekyll::Tags::PrettifyBlock)
